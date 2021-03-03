2903b8c1bf316567c612829a7fd69d4c535ae99c
PercentBoat4164 <thadd.schelp@gmail.com>
2021-03-03 00:46:12 -0500

    Used new methods for timing beats. Accuracy is about +/-1ms/beat. It tends to hover around the same place.

--- app/src/main/java/thadd/schelp/dynamicmetronome/MainActivity.kt
+++ app/src/main/java/thadd/schelp/dynamicmetronome/MainActivity.kt
@@ -1,22 +1,24 @@
 package thadd.schelp.dynamicmetronome
 
-import android.media.MediaPlayer
+import android.annotation.SuppressLint
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.SoundPool
 import android.os.*
 import android.util.Log
+import android.view.View
 import android.widget.CompoundButton
 import android.widget.NumberPicker
 import android.widget.SeekBar
-import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import thadd.schelp.dynamicmetronome.databinding.ActivityMainBinding
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
 
 const val MIN_TEMPO = 20
 const val MAX_TEMPO = 300F
 const val STARTING_TEMPO = 200
 
-var quarterPlayer = MediaPlayer()
+@SuppressLint("StaticFieldLeak")
+lateinit var appContext: Context
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainActivity: ActivityMainBinding
@@ -27,9 +29,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mainActivity = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainActivity.root)
-
-        quarterPlayer = MediaPlayer.create(this, R.raw.beep)
-        quarterPlayer.setVolume(1F, 1F)
+        appContext = applicationContext
+        metronome.generateSoundIDs()
 
         buildGUI()
     }
@@ -39,16 +40,16 @@ class MainActivity : AppCompatActivity() {
         mainActivity.Tempo.maxValue = MAX_TEMPO.toInt()
         mainActivity.Tempo.value = STARTING_TEMPO
         mainActivity.Tempo.wrapSelectorWheel = false
-        mainActivity.Tempo.displayedValues = Array<String>(MAX_TEMPO.toInt()){(it + MIN_TEMPO).toString()}
-        mainActivity.Tempo.setOnValueChangedListener { _: NumberPicker, _: Int, tempo: Int -> metronomeState.tempo = tempo; mainActivity.TempoSeekbar.progress = (tempo.toFloat() / MAX_TEMPO * 100).toInt() }
+        mainActivity.Tempo.displayedValues = Array(MAX_TEMPO.toInt()){(it + MIN_TEMPO).toString()}
+        mainActivity.Tempo.setOnValueChangedListener { _: NumberPicker, _: Int, tempo: Int -> metronomeState.setTempo(tempo); mainActivity.TempoSeekbar.progress = (tempo.toFloat() / MAX_TEMPO * 100).toInt() }
 
-        mainActivity.QuarterMute.setOnCheckedChangeListener{ _: CompoundButton, isChecked: Boolean -> if (isChecked) { quarterPlayer.setVolume(0F, 0F)} else { quarterPlayer.setVolume(mainActivity.QuarterVolume.progress.toFloat() / 100, mainActivity.QuarterVolume.progress.toFloat() / 100) } }
+        mainActivity.QuarterMute.setOnCheckedChangeListener{ _: CompoundButton, isChecked: Boolean -> if (isChecked) { metronome.setQuarterVolume(0F)} else { metronome.setQuarterVolume(mainActivity.QuarterVolume.progress / 100F) } }
 
         mainActivity.StartStopButton.setOnClickListener{ metronome.toggle() }
 
         mainActivity.QuarterVolume.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                quarterPlayer.setVolume(progress.toFloat() / 100, progress.toFloat() / 100.toFloat())
+                metronome.setQuarterVolume(progress / 100F)
                 mainActivity.QuarterMute.isChecked = false
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
@@ -59,7 +60,7 @@ class MainActivity : AppCompatActivity() {
         mainActivity.TempoSeekbar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 if (fromUser) {
-                    metronomeState.tempo = (progress * ((MAX_TEMPO - MIN_TEMPO) / 100)).toInt() + MIN_TEMPO
+                    metronomeState.setTempo((progress * ((MAX_TEMPO - MIN_TEMPO) / 100)).toInt() + MIN_TEMPO)
                     mainActivity.Tempo.value = metronomeState.tempo
                 }
             }
@@ -72,40 +73,50 @@ class MainActivity : AppCompatActivity() {
 
 class MetronomeState {
     var tempo = STARTING_TEMPO
+    var soundPool: SoundPool = SoundPool.Builder().setMaxStreams(1).setAudioAttributes(AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).setUsage(AudioAttributes.USAGE_GAME).build()).build()
+    var volume = 1F
+    var interval = (60000F / tempo).toLong()
+
+    @JvmName("setTempo1")
+    fun setTempo(newTempo: Int) {
+        tempo = newTempo
+        interval = (60000F / tempo).toLong()
+    }
+
+    @JvmName("setVolume1")
+    fun setVolume(newVolume: Float) {
+        volume = newVolume
+    }
 }
 
-class Metronome(metronomeState: MetronomeState) {
-    var state = metronomeState
-    var paused = true
-    var lock = Object()
-//    var scheduler = Executors.newScheduledThreadPool(1)
-    var quarterThread = Thread {
-        while (!Thread.interrupted()) {
-            while (paused) { synchronized(lock) { lock.wait() } }
-            quarterPlayer.seekTo(0)
-            quarterPlayer.start()
-            Thread.sleep((60000 / state.tempo).toLong())
-        }
-    }
-//    var quarterThread = Runnable( object: Runnable, () -> Unit {
-//        override fun run() {
-//            if (paused) {
-//                Log.d("", "Locked!")
-//                synchronized(lock) {lock.wait() } }
-//            quarterPlayer.seekTo(0)
-//            quarterPlayer.start()
-//            scheduler.schedule(this, (60000 / state.tempo).toLong(), TimeUnit.MILLISECONDS)
-//        }
-//    override fun invoke() {}
-//})
+class Metronome(metronomeState: MetronomeState) : Runnable{
+    private var isPlaying = true
+    private var state = metronomeState
+    private var handler = Handler(Looper.getMainLooper())
+    private var soundID = 0
 
     init {
-        quarterThread.start()
-//        scheduler.schedule(quarterThread, 0, TimeUnit.NANOSECONDS)
+        handler.post(this)
     }
 
     fun toggle() {
-        paused = !paused
-        synchronized(lock) { lock.notifyAll() }
+        isPlaying = !isPlaying
+    }
+
+    fun setQuarterVolume(volume: Float) {
+        state.setVolume(volume)
+    }
+
+    fun generateSoundIDs() {
+        soundID = state.soundPool.load(appContext, R.raw.beep, 1)
+    }
+
+    override fun run() {
+        handler.postDelayed(this, state.interval - 1)
+        if (isPlaying) {
+            state.soundPool.play(soundID, state.volume, state.volume, 0, 0, 2F)
+        } else {
+            state.soundPool.play(soundID, 0F, 0F, 0, 0, 2F)
+        }
     }
 }
