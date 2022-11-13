0b3c69bb9a6acc88c7fa989bbb3a7c9efe470e66
PercentBoat4164 <thadd.schelp@gmail.com>
2022-11-13 01:19:25 -0500

    - Fixed program sounds.

--- app/src/main/java/dynamicmetronome/metronome/Program.kt
+++ app/src/main/java/dynamicmetronome/metronome/Program.kt
@@ -11,7 +11,7 @@ class Program {
         else {
             instructions[bar] = Instruction(
               tempo,
-              if (interpolation) Double.POSITIVE_INFINITY else Double.NEGATIVE_INFINITY,
+              if (interpolation) Double.POSITIVE_INFINITY else 0.0,
               Long.MAX_VALUE)
         }
     }
@@ -20,33 +20,29 @@ class Program {
         val result = ArrayList<Double>()
         val entries = getInstructionsAndBars()
         var tempo = entries[0].second.startTempo
-        for (entry in entries) {
-            for (i in 0 until entry.second.beats) {
+        for (i in 0 until entries.size) {
+            for (j in 0 until entries[i].second.beats.toInt()) {
                 result.add(tempo)
-                tempo += entry.second.tempoOffset
+                tempo += entries[i].second.tempoOffset
             }
         }
         return result.toTypedArray().toDoubleArray()
     }
 
     fun getInstructionsAndBars(): ArrayList<Pair<Long, Instruction>> {
-        val entries = ArrayList(instructions.toSortedMap().toList())
+        // Force Kotlin to perform a deep copy. I hate you Java! Why must you always pass by reference?
+        val temp: MutableMap<Long, Instruction> = HashMap()
+        instructions.forEach{(key, value) -> temp[key] = Instruction(value.startTempo, value.tempoOffset, value.beats)}
+        val entries = ArrayList(temp.toSortedMap().toList())
         if (entries.size > 0) {  // If instructions exist, build them.
-            entries.add(0, Pair(0, Instruction(entries[0].second.startTempo, 0.0, entries[0].first * 4)))  // 4 as in 4/4 time signature
+            if (entries[0].first != 0L) entries.add(0, Pair(0, Instruction(entries[0].second.startTempo, 0.0, 0)))  // 4 as in 4/4 time signature
             for (entry in 1 until entries.size) {
-                try {
-                    entries[entry].second.beats = (entries[entry].first - entries[entry - 1].first) * 4  // 4 as in 4/4 time signature
-                    if (entries[entry].second.tempoOffset == Double.POSITIVE_INFINITY)
-                        entries[entry].second.tempoOffset = entries[entry].second.startTempo - entries[entry - 1].second.startTempo
-                    else entries[entry].second.tempoOffset = 0.0
-                } catch (_: java.lang.IndexOutOfBoundsException) {
-                    entries[entry].second.beats = entries[entry].first * 4  // 4 as in 4/4 time signature
-                    if (entries[entry].second.tempoOffset == Double.POSITIVE_INFINITY)
-                        entries[entry].second.tempoOffset = entries[entry].second.startTempo
-                    else entries[entry].second.tempoOffset = 0.0
-                }
+                entries[entry - 1].second.beats = (entries[entry].first - entries[entry - 1].first) * 4  // 4 as in 4/4 time signature
+                if (entries[entry].second.tempoOffset != 0.0)
+                    entries[entry - 1].second.tempoOffset = (entries[entry].second.startTempo - entries[entry - 1].second.startTempo) / (entries[entry - 1].second.beats)
             }
             entries.last().second.beats = 1
+            entries.last().second.tempoOffset = 0.0
         }
         return entries
     }
